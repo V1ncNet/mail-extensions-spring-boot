e1286d2644054c6d82028fc49ed886a921594020
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-03-05 16:11:09 +0100

    Load private key from file URI or classpath
    refs #1

--- spring-boot-autoconfigure/src/main/java/de/vinado/boot/autoconfigure/dkim/DkimSignerAutoConfiguration.java
+++ spring-boot-autoconfigure/src/main/java/de/vinado/boot/autoconfigure/dkim/DkimSignerAutoConfiguration.java
@@ -2,6 +2,8 @@ package de.vinado.boot.autoconfigure.dkim;
 
 import de.vinado.spring.mail.javamail.dkim.DkimJavaMailSender;
 import net.markenwerk.utils.mail.dkim.DkimSigner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -11,8 +13,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.mail.javamail.JavaMailSender;
 
 import java.io.IOException;
@@ -34,11 +38,21 @@ import java.util.Properties;
 @ConditionalOnProperty(prefix = "dkim", name = {"selector", "signing-domain", "private-key"})
 class DkimSignerAutoConfiguration {
 
+    public static final Logger log = LoggerFactory.getLogger(DkimSignerAutoConfiguration.class);
+
     @Bean
     @Conditional(PrivateKeyNotEmpty.class)
     @ConditionalOnMissingBean
     DkimSigner dkimSigner(DkimProperties properties) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException {
-        Resource resource = new FileSystemResource(properties.getPrivateKey());
+        String location = properties.getPrivateKey();
+        ResourceLoader loader = new DefaultResourceLoader();
+        Resource resource = loader.getResource(location);
+
+        if (resource.getClass().getSimpleName().equals("ClassPathContextResource")) {
+            log.warn("Referencing property [dkim.private-key] with an absolute path is deprecated. You should use the classpath: or file: protocols.");
+            resource = new FileSystemResource(location);
+        }
+
         DkimSigner signer = new DkimSigner(properties.getSigningDomain(), properties.getSelector(), resource.getInputStream());
         applyProperties(properties, signer);
         return signer;
---
+++ spring-boot-autoconfigure/src/test/java/de/vinado/boot/autoconfigure/dkim/DkimSignerAutoConfigurationTest.java
@@ -0,0 +1,53 @@
+package de.vinado.boot.autoconfigure.dkim;
+
+import net.markenwerk.utils.mail.dkim.DkimSigner;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Vincent Nadoll
+ */
+class DkimSignerAutoConfigurationTest {
+
+    private static final String PWD = System.getProperty("user.dir");
+
+    private final ApplicationContextRunner contextRunner = new ApplicationContextRunner()
+        .withConfiguration(AutoConfigurations.of(DkimSignerAutoConfiguration.class));
+
+    @Test
+    void missingProperty_shouldNotCreateDkimSignerBean() {
+        this.contextRunner
+            .withPropertyValues("dkim.signing-domain=domain.tld")
+            .run(context -> assertThat(context).doesNotHaveBean(DkimSigner.class));
+    }
+
+    @Test
+    void absoluteKeyLocation_shouldCreateDkimSignerBean() {
+        assertSuccessfulDkimBeanCreation(String.format("%s/src/test/resources/test.key.der", PWD));
+    }
+
+    @Test
+    void fileUri_shouldCreateDkimSignerBean() {
+        assertSuccessfulDkimBeanCreation("file:src/test/resources/test.key.der");
+    }
+
+    @Test
+    void classpathUri_shouldCreateDkimSignerBean() {
+        assertSuccessfulDkimBeanCreation("classpath:test.key.der");
+    }
+
+    private void assertSuccessfulDkimBeanCreation(String privateKeyLocation) {
+        String[] properties = {
+            "dkim.signing-domain=domain.tld",
+            String.format("dkim.private-key=%s", privateKeyLocation),
+            "dkim.selector=default"
+        };
+
+        this.contextRunner
+            .withPropertyValues(properties)
+            .run(context -> assertThat(context).hasSingleBean(DkimSigner.class));
+    }
+}
